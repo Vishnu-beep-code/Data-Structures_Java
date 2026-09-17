31b798271797528877be1a140ae4828a2716b41f
Anand <vishnuanand0775@gmail.com>
2026-09-17 16:35:48 +0000

    ...

---
+++ Home Work/HWcode5.java
@@ -0,0 +1,32 @@
+class Solution {
+
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+        for (int i = 0; i < nums.length; i++) {
+
+            for (int j = i + 1; j < nums.length; j++) {
+
+                if (nums[i] == nums[j]) {
+
+                    if (j - i <= k) {
+                        return true;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        Solution obj = new Solution();
+
+        int[] nums = {1, 2, 3, 1};
+        int k = 3;
+
+        boolean result = obj.containsNearbyDuplicate(nums, k);
+
+        System.out.println(result);
+    }
+}
