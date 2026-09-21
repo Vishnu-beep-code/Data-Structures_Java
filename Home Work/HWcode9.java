b957ab59477fecfb7d7f11823767f6c6aec25abb
Anand <vishnuanand0775@gmail.com>
2026-09-21 14:32:58 +0000

    ...

--- Home Work/HWcode9.java
+++ Home Work/HWcode9.java
@@ -1,3 +1,34 @@
-public class HWcode9 {
-    
+import java.util.*;
+
+class Solution {
+
+    public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int needed = target - nums[i];
+
+            if (map.containsKey(needed)) {
+                return new int[] { map.get(needed), i };
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[] {};
+    }
+
+    public static void main(String[] args) {
+
+        Solution obj = new Solution();
+
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] result = obj.twoSum(nums, target);
+
+        System.out.println("Output: [" + result[0] + ", " + result[1] + "]");
+    }
 }
