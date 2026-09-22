da65289ff4105b48dcd52b05a4ad58329a227a55
Anand <vishnuanand0775@gmail.com>
2026-09-22 09:10:58 +0000

    ...

--- Home Work/HWcode10.java
+++ Home Work/HWcode10.java
@@ -10,26 +10,18 @@ class Main {
         long maxSum = 0;
 
         for (int i = 0; i < nums.length; i++) {
-
-            // Remove the left element if it is already in the window
             while (set.contains(nums[i])) {
                 int left = nums[i - set.size()];
                 set.remove(left);
                 sum -= left;
             }
-
-            // Add current element
             set.add(nums[i]);
             sum += nums[i];
-
-            // Keep window size k
             if (set.size() > k) {
                 int left = nums[i - k];
                 set.remove(left);
                 sum -= left;
             }
-
-            // Check valid window
             if (set.size() == k) {
                 maxSum = Math.max(maxSum, sum);
             }
